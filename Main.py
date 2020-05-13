d4f2528114ec74f8472f653ed3e7acb292d21709
gunwoo7 <b9635005@naver.com>
2020-05-13 09:27:47 +0900

    Add base code + Edit gitignore for save secret dat

---
+++ Main.py
@@ -0,0 +1,30 @@
+import asyncio
+import discord
+
+app = discord.Client()
+
+def get_token():
+    global token
+    f = open("Key.key", "r")
+    token = str(f.readline())
+
+@app.event
+async def on_ready():
+    print("다음으로 로그인합니다 : ")
+    print(app.user.name)
+    print(app.user.id)
+    print("==========")
+    game = discord.Game("Koi Bot이 정상 실행 중입니다!")
+    await app.change_presence(status=discord.Status.online, activity=game)
+
+
+
+@app.event
+async def on_message(message):
+    if message.author.bot:
+        return None
+    if message.content == "!안녕":
+        await message.channel.send("안녕하세요.")
+
+get_token()
+app.run(token)
