e0201dd49ce566ae5701216c697625555e202323
aheui-lover3125 <b9635005@naver.com>
2020-09-13 23:16:21 +0900

    Change main.py to load AI_Chat.py Load Kogpt2 func in main.py to use kogpt2 in koi bot

--- Main.py
+++ Main.py
@@ -1,6 +1,11 @@
 import asyncio
 import discord
-app = discord.Client()
+from discord.ext import commands
+import sys
+sys.path.insert(1, 'KoGPT2-chatbot')
+import AI_talk as kogpt2
+
+app = commands.Bot(command_prefix = "//")
 
 def get_token():
     global token
@@ -15,13 +20,16 @@ async def on_ready():
     print("==========")
     game = discord.Game("Koi Bot이 정상 실행 중입니다!")
     await app.change_presence(status=discord.Status.online, activity=game)
-
-
+    kogpt2.Load_Model()
 
 @app.event
 async def on_message(message):
+    await app.process_commands(message)
     if message.author.bot:
         return None
+    if message.content[:4] == "코이야 ":
+        await message.channel.send(kogpt2.Chat_To_AI(message.content[4:]))
+        print(message.content)
     
 
 get_token()
