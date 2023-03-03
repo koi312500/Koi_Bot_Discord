d8ea73db1aba8780a560a6c92e1893cd1461e237
koi312500 <koi312500@gmail.com>
2023-03-03 19:14:56 +0900

    Auto School Info Command Update

--- Cogs/Tools.py
+++ Cogs/Tools.py
@@ -1,97 +1,54 @@
 import asyncio
-from dis import show_code
-from re import search
 import discord
+import requests
 from discord.commands import slash_command
 from discord.ext import commands
 from discord.ext import tasks
 
-from hcspy import HCSClient
 import pickle
-import time
+import pytz
+from datetime import datetime
+import dateutil.tz as gettz
 
+import config
 from Utils import Logger
 from Utils import Permission
 
 class Tools(commands.Cog):
-
     def __init__(self, app):
         self.app = app
-        self.selfcheck_loop.start()
+        self.school_loop.start()
 
     @tasks.loop(hours = 1)
-    async def selfcheck_loop(self):
+    async def school_loop(self):
         await self.app.wait_until_ready()
-        if str(time.localtime().tm_hour) == "6":
-            with open("Data/selfcheck.dat", "rb") as selfcheck_data:
-                selfcheck_list = pickle.load(selfcheck_data)
-            for i in list(selfcheck_list.keys()):
-                if str(selfcheck_list[str(i)]['error']) == "True":
-                    continue
-                result = await hcskr.asyncTokenSelfCheck(str(selfcheck_list[str(i)]['token']))
+        TIME_ZONE = pytz.timezone('Asia/Seoul')
+        currentTime = datetime.now(TIME_ZONE)
+        if int(currentTime.hour) == 6:
+            with open("Data/SchoolInfo.dat", "rb") as school_data:
+                school_member = pickle.load(school_data)
+            for i in list(school_member.keys()):
                 dm_user = await self.app.fetch_user(int(i))
-                embed = discord.Embed(title=f"Covid19 Auto Selfcheck Result", color=0x0AB1C2)
-                embed.set_footer(text=f"Sented by Koi_Bot#4999ㆍAM 06:00 ~ AM 07:00 Auto COVID19 SelfCheck")
-                embed.add_field(name = "Running Info", value = f"Code '{result['code']}' is returned.", inline = False)
-                embed.add_field(name = "Code Info", value = f"{result['message']}", inline = False)
-                embed.add_field(name = "Executed Time", value = f"Executed at {result['regtime']}.", inline = False)
-                await dm_user.send(embed=embed)
-            await Logger.info("Auto Covid19 Selfcheck executed.", self.app)    
+                Food_type = ["아침", "점심", "저녁"]
+                embed = discord.Embed(title=f"대전과학고 정보 알리미!", color=0x0AB1C2)
+                embed.set_footer(text=f"Sented by Koi_Bot#4999ㆍAM 06:00 ~ AM 07:00 Auto School Info Command")
+                url = config.meal_URL + str(currentTime.year) + str(currentTime.month).zfill(2) + str(currentTime.day).zfill(2) + config.meal_key
+                data = requests.get(url).json()
+                for j in range(0, 3):
+                    now_check = str(data['mealServiceDietInfo'][1]['row'][j]['DDISH_NM'].replace('<br/>', '\n'))
+                    embed.add_field(name = "급식 : " + str(Food_type[j]), value = now_check, inline = False)
+                await dm_user.send(embed = embed)
 
-
- 
-    @slash_command(name = "selfcheck", guild_ids = [742201063972667487])
-    async def selfcheck_command(self, ctx, name = None, birthday = None, school_name = None, password = None):
+    @slash_command(name = "register_school", guild_ids = [742201063972667487])
+    async def register_school_command(self, ctx, class_num : int):
         if await Permission.check_permission(ctx, 1):
             return None
-        with open("Data/selfcheck.dat", "rb") as selfcheck_data:
-            selfcheck_list = pickle.load(selfcheck_data)
-        if name == None:
-            if str(ctx.author.id) not in selfcheck_list:
-                embed = discord.Embed(title=f"자가진단 정보 등록법", color=0x0AB1C2)
-                embed.set_footer(text=f"Sented by Koi_Bot#4999ㆍselfcheck Command's Result")
-                embed.add_field(name = "How to register", value = "/selfcheck 이름 생년월일(YYMMDD) 학교이름 비밀번호(예제와 동일한 형식으로 입력해주세요.)", inline = False)
-                embed.add_field(name = "Ex", value = "/selfcheck 홍길동 010101 서울중학교 1111", inline = False)
-                embed.add_field(name = "Security", value = "모든 데이터는 평문의 형태로 저장되며, Koi_Bot#4999 및 KOI#4182가 이 데이터에 접근 할 수 있습니다.", inline = False)
-                await ctx.respond(embed=embed)    
-            else:
-                result = await hcskr.asyncTokenSelfCheck(str(selfcheck_list[str(ctx.author.id)]['token']))
-                embed = discord.Embed(title=f"Covid19 Auto Selfcheck Result", color=0x0AB1C2)
-                embed.set_footer(text=f"Sented by Koi_Bot#4999ㆍselfcheck Command's Result")
-                embed.add_field(name = "Running Info", value = f"Code '{result['code']}' is returned.", inline = False)
-                embed.add_field(name = "Code Info", value = f"{result['message']}", inline = False)
-                embed.add_field(name = "Executed Time", value = f"Executed at {result['regtime']}.", inline = False)
-                await ctx.respond(embed=embed) 
+        with open("Data/SchoolInfo.dat", "rb") as school_data:
+            school_member = pickle.load(school_data)
+        school_member[str(ctx.author.id)] = class_num
+        with open("Data/SchoolInfo.dat", "wb") as school_data:
+            pickle.dump(school_member, school_data)
         
-        elif str(name) != "all":
-            if birthday == None or school_name == None or password == None:
-                await ctx.respond("무언가 빠졌어요! 다시 입력해주세요!", ephemeral = True)
-            else:
-                hcspy_client = HCSClient()
-                hcspy_user_info = [str(name), str(birthday), str(school_name), str(password)]
-                selfcheck_list[str(ctx.author.id)] = hcspy_user_info
-                hcspy_user = await HCSClient.login_fast(name = name, school_name = school_name, birthday = birthday, password = password)
-                print(await hcspy_user[-1].check())
-                await ctx.respond(hcspy_user_info, ephemeral = True)
-                with open("Data/selfcheck.dat", "wb") as selfcheck_data:
-                    pickle.dump(selfcheck_list, selfcheck_data)
-        
-        else:
-            await Logger.info(f"Covid19 Selfcheck executed by {str(ctx.author)}. (It was run manually.)", self.app)
-            with open("Data/selfcheck.dat", "rb") as selfcheck_data:
-                selfcheck_list = pickle.load(selfcheck_data)
-            for i in list(selfcheck_list.keys()):
-                if str(selfcheck_list[str(i)]['error']) == "True":
-                    continue
-                result = await hcskr.asyncTokenSelfCheck(str(selfcheck_list[str(i)]['token']))
-                dm_user = await self.app.fetch_user(int(i))
-                embed = discord.Embed(title=f"Covid19 Auto Selfcheck Result", color=0x0AB1C2)
-                embed.set_footer(text=f"Sented by Koi_Bot#4999ㆍAuto COVID19 SelfCheck Executed by {str(ctx.author)}")
-                embed.add_field(name = "Running Info", value = f"Code '{result['code']}' is returned.", inline = False)
-                embed.add_field(name = "Code Info", value = f"{result['message']}", inline = False)
-                embed.add_field(name = "Executed Time", value = f"Executed at {result['regtime']}.", inline = False)
-                embed.add_field(name = f"Executed by {str(ctx.author)}", value = "Maybe AutoSelfCheck would not be executed Automatically. (It was run manually.)", inline = False)
-                await dm_user.send(embed=embed)
 
 def setup(app):
     app.add_cog(Tools(app))
