9a662c73e7303a6ecdbe99c1a99e5bdbfc291f16
KOI3125 <aheui.lover3125@gmail.com>
2023-05-08 23:28:05 +0900

    Update for the server(Laptop)

---
+++ Cogs/LaptopManagement.py
@@ -0,0 +1,44 @@
+from discord.ext import commands
+from discord.ext import tasks
+from discord.commands import slash_command
+
+import psutil
+import socket
+
+from Utils import Permission
+from Utils import Logger
+
+class LapTopManagement(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.battery_status_check.start()
+
+    @tasks.loop(minutes = 5)
+    async def battery_status_check(self):
+        battery = psutil.sensors_battery()
+        if battery.power_plugged is not True:
+            channel = await self.bot.fetch_channel(865999145600286741)
+            await channel.send(f"Laptop's battery status : {battery.percent}%, Power : {battery.power_plugged}")
+            await channel.send(f"Warning!!! Warning Message to : <@753625063357546556>\nBattery is unplugged, please shutdown the computer or solve the problem please.")
+        
+    @slash_command(name = "battery_info", guild_ids = [742201063972667487])
+    async def battery_info_command(self, ctx):
+        if await Permission.check_permission(ctx, 3):
+            return None
+
+        battery = psutil.sensors_battery()
+        await ctx.respond(f"Laptop's battery status : {battery.percent}%, Power : {battery.power_plugged}")
+
+    @slash_command(name = "ip_info", guild_ids = [742201063972667487])
+    async def ip_info_command(self, ctx):
+        if await Permission.check_permission(ctx, 3):
+            return None
+
+        hostname = socket.gethostname()
+        IpAddr = socket.gethostbyname(hostname)
+        await ctx.respond(f"Computer's name : {hostname}\nComputer's IP : {IpAddr}")
+                        
+
+def setup(bot):
+    bot.add_cog(LapTopManagement(bot))
