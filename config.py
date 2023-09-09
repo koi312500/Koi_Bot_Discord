330d31b5d758cb38a67fdfc18962b276e746abec
koi312500 <koi312500@gmail.com>
2023-09-09 17:06:03 +0900

    LaptopManagement Disable at Server

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from key import *
 
 #Bot Config
 bot_name = "Koi_Bot"
-now_ver = "1.6.0"
+now_ver = "1.6.1"
 status_list = [f"Made by @koi3125 (Ver {now_ver})", f"`/invite` to join 『Hello, Discord!』 Server! (Ver {now_ver})"]
 
 #Debug Config
@@ -13,4 +13,4 @@ debug_bot_name = "Debug_Koi_Bot"
 debug_status_list = [f"Debugging Koi_Bot... (Ver {now_ver})", f"Debugged by @koi3125 (Ver {now_ver})"]
 
 #Environment Config
-Laptop = True
+Laptop = False
