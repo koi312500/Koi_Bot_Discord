cc8edf6dd638f8bc7c7797a2067b8a3b83d2dc3c
KOI3125 <koi312500@gmail.com>
2023-10-30 22:45:31 +0900

    fix: resolve file name

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@ echo "This is Koi_Bot launcher. It automatically restart Koi_Bot."
 while :
 do
     echo "Koi_Bot's Main.py Started."
-    xvfb-run python qMain.py
+    xvfb-run python Main.py
 done
