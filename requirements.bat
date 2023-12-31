e02ba326b45464c03111657464a6afcddb3023d8
민재 유 <111565097+arpa0091@users.noreply.github.com>
2023-12-31 13:11:06 +0900

    installer

---
+++ requirements.bat
@@ -0,0 +1,8 @@
+pip install py-cord
+pip install pytz
+pip install python-dateutil
+pip install psutil
+pip install selenium
+pip install pyautogui
+pip install requests
+pip install webdriver-manager
