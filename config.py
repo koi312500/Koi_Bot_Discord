610f4cf52ac296db5c8e827aec3f7a1bbb106568
KOI3125 <koi312500@gmail.com>
2024-02-03 15:53:07 +0900

    fix: remove LaptopManagement from cogs exception

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from key import *
 
 #Bot Config
 bot_name = "Koi_Bot"
-now_ver = "1.7.8"
+now_ver = "1.7.10"
 status_list = [f"Made by @koi3125 (Ver {now_ver})", f"`/invite` to join 『Hello, Discord!』 Server! (Ver {now_ver})"]
 
 #Debug Config
@@ -13,4 +13,4 @@ debug_bot_name = "Debug_Koi_Bot"
 debug_status_list = [f"Debugging Koi_Bot... (Ver {now_ver})", f"Debugged by @koi3125 (Ver {now_ver})"]
 
 #Environment Config
-Cogs_Excepction = ["Tools", "LaptopManagement"]
+Cogs_Excepction = ["Tools"]
